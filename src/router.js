7681f4f53453b6779ce65d60e58a8e491b741e42
piepolina <piemekanika@hotmail.com>
2019-02-11 00:32:22 +0700

    Update router.js
    router.js updated

--- src/router.js
+++ src/router.js
@@ -16,25 +16,16 @@ export default new Router({
     {
       path: '/portfolio',
       name: 'portfolio',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Portfolio.vue')
+      component: () => import(/* webpackChunkName: "portfolio" */ './views/Portfolio.vue')
     },
     {
       path: '/team',
       name: 'team',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Team.vue')
+      component: () => import(/* webpackChunkName: "team" */ './views/Team.vue')
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
