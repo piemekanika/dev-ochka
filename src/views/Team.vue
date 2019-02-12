fc0b9abf0f790bd8885a421046ee7b6f83d820a6
piepolina <piemekanika@hotmail.com>
2019-02-12 20:34:17 +0700

    started adding lines to team page

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,4 +2,14 @@
     .container.team-page
         h3 Team page
             span(style="color: green; font-family: monospace; font-size: 20px; padding-left: 15px;") //TODO
+
+            .line(style="")
 </template>
+
+<style lang="stylus" scoped>
+    .line
+        position absolute
+        width 1px
+        height 100px
+        border 1px solid gray
+</style>
