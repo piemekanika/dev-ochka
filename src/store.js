4a3db841bc384bc01d29e3209546502f3006645b
piepolina <piemekanika@hotmail.com>
2019-02-23 17:13:17 +0700

    gived up with vuex mouse coords

--- src/store.js
+++ src/store.js
@@ -5,25 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        mouse: {
-            x: null,
-            y: null
-        }
+
     },
 
     mutations: {
-        setMousePosition (state, payload) {
-            state.mouse.x = payload.x
-            state.mouse.y = payload.y
-        }
+        
     },
     
     actions: {
-        updateMousePosition (mouseEvent) {
-            store.commit('setMousePosition', {
-                x: mouseEvent.pageX,
-                y: mouseEvent.pageY
-            })
-        }
+        
     }
 })
